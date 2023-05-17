6c9e1165d8042c69771a22118f45e4b5d089c495
MundatiC <calebngethe230@gmail.com>
2023-05-17 08:35:43 +0300

    making changes to hide_email.js

--- hide_email.js
+++ hide_email.js
@@ -3,7 +3,7 @@ function hide_email(email) {
     const [username, domain] = email.split("@");
   
    
-    const hiddenUsername = username.slice(0, 3) + "...";
+    const hiddenUsername = username.slice(0, 5) + "...";
   
     
     const hiddenEmail = hiddenUsername + "@" + domain;
@@ -15,4 +15,5 @@ function hide_email(email) {
   console.log(hide_email("robin_singh@example.com"));  
   console.log(hide_email("john.doe@gmail.com"));   
   console.log(hide_email("alice.smith@company.com"));
+  console.log(hide_email("calebngethe230@gmail.com"));
   
